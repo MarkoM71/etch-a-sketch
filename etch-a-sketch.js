74c86d9e45e9a62b83b45423e9407614567c0d05
Marko Matura <Marko.Matura@gmail.com>
2023-01-11 16:46:58 -0500

    Add function to create Grid using CSS and JavaScript.

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -1,30 +1,6 @@
 const container = document.querySelector('.container')
 
-function addDivs(rows, cols){
-    container.style.setProperty('--grid-rows', rows);
-    container.style.setProperty('--grid-cols', cols);
-    for (let i = 0; i < rows * cols; i++) {
-        const content = document.createElement('div');
-        content.classList.add('item');
-        container.appendChild(content);
-    }
-    changeColor();
-}
-
-function changeColor() {
-    const etchs = document.querySelectorAll('.item');
-    etchs.forEach(etch => {
-        etch.addEventListener('mouseover', () => {
-            etch.style.backgroundColor = 'red';
-        });
-    });
-}
-
-addDivs(16, 16);
-
-/*let number = 16;
-
-container.style.setProperty('grid-column', number);
+let number = 16;
 
 let grid = number * number
 
@@ -32,10 +8,18 @@ for (let i = 0; i < grid; i++) {
 
     const content = document.createElement('div');
     content.classList.add('item');
+
     container.appendChild(content);
 
-}*/
+}
 
+const etchs = document.querySelectorAll('.item');
+
+etchs.forEach(etch => {
+    etch.addEventListener('mouseover', () => {
+    etch.style.backgroundColor = 'red';
+    });
+});
 
 
 /*for (let j = 0; j < etchs.length; j++) {
