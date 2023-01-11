6952206779c143ae6b0e114e04d09b899783ba2f
Marko Matura <Marko.Matura@gmail.com>
2023-01-11 15:12:03 -0500

    Add feature for color change on etch-a-sketch.

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -6,11 +6,26 @@ let grid = number * number
 
 for (let i = 0; i < grid; i++) {
 
-
     const content = document.createElement('div');
     content.classList.add('item');
-    
 
     container.appendChild(content);
 
 }
+
+const etchs = document.querySelectorAll('.item');
+
+etchs.forEach(etch => {
+    etch.addEventListener('mouseover', () => {
+    etch.style.backgroundColor = 'red';
+    });
+});
+
+
+/*for (let j = 0; j < etchs.length; j++) {
+    etchs[j].addEventListner('mouseover', () => {
+        etchs[j].style.backgroundColor = 'blue'; 
+    });
+}*/
+
+
